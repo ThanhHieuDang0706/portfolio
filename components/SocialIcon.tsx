1b02caf2e75fa95ab2e69d1555854ffbb0be8339
Hieu Dang <hieu.dt@geekup.vn>
2022-08-13 21:51:41 +0700

    add footer

---
+++ components/SocialIcon.tsx
@@ -0,0 +1,10 @@
+
+import { SocialIcon as ReactSocialIcon} from "react-social-icons";
+
+const SocialIcon = props => {
+
+
+   return <ReactSocialIcon {...props} className="m-4 h-8 w-8"/>
+}
+
+export default SocialIcon;
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,24 +1,57 @@
+import SocialIcon from "./SocialIcon";
+import { FaPhoneAlt, FaMailBulk, FaRegCopyright } from "react-icons/fa";
+
 const Footer = () => {
+
   return (
     <footer className="bg-gray-50 border-t py-12 h-32">
       <div className="max-w-5xl mx-auto flex justify-between">
-        <div className="flex items-center space-x-4">
-          <img
-            src="/react-bricks-icon.svg"
-            alt="React Bricks"
-            className="w-8"
-          />
-          <div className="uppercase text-sm tracking-wider">Next.js website starter</div>
+        <div className="text-sm">
+          <h2>Contact me</h2>
+
+          <div className="flex mx-auto my-2">
+            <div className="mr-2">
+              <FaPhoneAlt size="19" className="nav-linker" />
+            </div>
+
+            <div>
+              <a href="tel:+84522150701">+84 522 150 701</a>
+            </div>
+          </div>
+
+          <div className="flex mx-auto my-2 justify-between">
+            <div className="mr-2">
+              <FaMailBulk size="19" className="nav-linker" />
+            </div>
+
+            <div>
+              <a href="mailto:thanhhieu0706@outlook.com">
+                {"thanhhieu0706@outlook.com".toLowerCase()}
+              </a>
+            </div>
+          </div>
+
+          <div className="flex mx-auto my-2">
+            <div className="text-sm mr-2">
+              <FaRegCopyright size="19" className="nav-linker" />
+            </div>
+
+            <div>
+              <a href="#" className="hover:text-pink-600">
+                {new Date().getFullYear()} Hieu Dang
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="text-gray-500 text-sm">
-          © {new Date().getFullYear()}{' '}
-          <a href="https://reactbricks.com" className="hover:text-pink-600">
-            React Bricks
-          </a>
+
+        <div>
+          <SocialIcon url="https://www.linkedin.com/in/hieu-dang-b9800a184/" />
+          <SocialIcon url="https://github.com/ThanhHieuDang0706" />
+          <SocialIcon url="mailto:thanhhieu0707@outlook.com" />
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
